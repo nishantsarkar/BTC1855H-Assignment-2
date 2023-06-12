6d90a90089cab507a8d0f0fca19def5aa5dc12a0
Nishant <nsrk01@gmail.comm>
2023-06-12 09:39:59 -0400

    Expanded notes in code

--- Assignment2_Sarkar.R
+++ Assignment2_Sarkar.R
@@ -1,14 +1,20 @@
 # A script that identifies whether user-input numbers are Armstrong numbers
 
-# Prompting user to enter three-digit number
+#' Prompting user to enter three-digit number. This is assigned to the variable
+#' userinput. 
 userinput <- readline("Please enter a positive three-digit number: ")
 
-# Checking if the user input is numeric, positive and three digits
+#' Checking if the user input is numeric, positive and three digits. An input 
+#' number must fulfill all three conditions before progressing. The first
+#' line checks if the input is a number using is.numeric. The second
+#' checks if the number of characters in userinput is exactly 3. The third
+#' converts userinput to a number, then checks if it is positive.  
 if ((is.numeric(as.numeric(userinput))) &
    (nchar(userinput) == 3) &  
    (as.numeric(userinput) >= 0)) { 
      
-  # Using strsplit to split userinput into vector of individual digits 
+  #' Using strsplit to split userinput into vector of individual digits. This
+  #' vector is then converted to a number and assigned to the variable splitdigits. 
   splitdigits <- as.numeric(strsplit(userinput, "")[[1]])
   # Finding the sum of cubes of the individual digits in userinput
   cubesum <- sum(splitdigits^3)
