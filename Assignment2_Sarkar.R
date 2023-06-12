937ee0f9cf87248169de08c0c8c066dac66dc7a8
Nishant <nsrk01@gmail.comm>
2023-06-12 08:11:29 -0400

    Outer if-else block checking whether input number is numeric, positive and three digits

--- Assignment2_Sarkar.R
+++ Assignment2_Sarkar.R
@@ -1,5 +1,18 @@
 # A script that identifies whether user-input numbers are Armstrong numbers
 
 # Prompting user to enter three-digit number
-userinput <- readline("Please enter a three digit number: ")
+userinput <- readline("Please enter a positive three-digit number: ")
 
+# Checking if the user input is numeric, positive and three digits
+if ((is.numeric(as.numeric(userinput))) &
+   (nchar(userinput) == 3) &  
+   (as.numeric(userinput) >= 0)) { 
+     
+   print("Valid input!")
+
+     
+
+} else {
+  print("Invalid entry! Terminating...")
+  quit()
+} # Outer if-else checking for input validity
