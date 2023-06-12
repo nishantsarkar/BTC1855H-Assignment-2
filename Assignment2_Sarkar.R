a94edbc2b51ec23269d5574419e2a4e0ba2a09d2
Nishant <nsrk01@gmail.comm>
2023-06-12 07:57:44 -0400

    added user input using readline()

--- Assignment2_Sarkar.R
+++ Assignment2_Sarkar.R
@@ -1,2 +1,5 @@
 # A script that identifies whether user-input numbers are Armstrong numbers
 
+# Prompting user to enter three-digit number
+userinput <- readline("Please enter a three digit number: ")
+
